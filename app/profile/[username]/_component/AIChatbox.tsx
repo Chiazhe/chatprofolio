29c8c5753e01e130a5d8fcf334a4324c483334ff
“Your <chiazhe7@gmail.com>
2024-06-30 17:38:06 +0800

    Fixed linting error

--- app/profile/[username]/_component/AIChatbox.tsx
+++ app/profile/[username]/_component/AIChatbox.tsx
@@ -7,18 +7,8 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 type Props = { username: string };
 
